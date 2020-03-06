7809624b5c36fb1c006eb116f07daa9135b96af3
Gwak, Okdong <gwak.okdong@gmail.com>
2020-03-06 21:05:36 +0900

    Initial commit

---
+++ src/test/java/io/github/bissal/springboot2webservice/Springboot2WebserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.bissal.springboot2webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2WebserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
